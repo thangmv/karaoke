9032c183980b46426eb4cca054fa66981edb6445
hungnm58 <hungnm58.work@gmail.com>
2016-11-14 15:57:29 +0700

    ## hungnm58 ## 14/11/2016 ## Finish footer

--- wp-content/themes/karaoke/footer.php
+++ wp-content/themes/karaoke/footer.php
@@ -14,11 +14,25 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'karaoke' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'karaoke' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'karaoke' ), 'karaoke', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
+		<div class="container">
+			<div class="footer">
+				<div class="row">
+					<div class="col-md-4 footer-1">
+						<?php dynamic_sidebar( 'footer-1' ); ?>
+					</div>
+					<div class="col-md-4 footer-2">
+						<?php dynamic_sidebar( 'footer-2' ); ?>
+					</div>
+					<div class="col-md-4 footer-3">
+						<?php dynamic_sidebar( 'footer-3' ); ?>
+						<?php echo do_shortcode('[contact-form-7 id="9" title="Subscrible footer"]') ?>
+					</div>
+				</div>
+			</div>
+			<div class="copyright">
+				<?php dynamic_sidebar( 'copyright' ); ?>
+			</div>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
--- wp-content/themes/karaoke/inc/custom-function.php
+++ wp-content/themes/karaoke/inc/custom-function.php
@@ -9,8 +9,8 @@
 /**
  * Add style file
  */
-if( !function_exists('register_style') ) {
-    function register_style () {
+if( !function_exists('karaoke_register_style') ) {
+    function karaoke_register_style () {
         // Custom style
         wp_enqueue_style( 'html-css', get_template_directory_uri() . '/assets/css/style.css' );
         wp_enqueue_style( 'custom', get_template_directory_uri() . '/assets/css/custom.css' );
@@ -18,9 +18,61 @@ if( !function_exists('register_style') ) {
         // Jquery
         wp_enqueue_script( 'jquery', get_template_directory_uri() . '/assets/js/jquery-3.1.1.min.js', array('jquery'), '20151215', true );
 
-        //Bootstrap
+        // Bootstrap
         wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css' );
         wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array('jquery'), '20151215', true );
+
+        // Font Awesome
+        wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/assets/css/font-awesome.min.css' );
     }
-    add_action( 'wp_enqueue_scripts', 'register_style' );
+    add_action( 'wp_enqueue_scripts', 'karaoke_register_style' );
+}
+
+if( !function_exists('karaoke_widgets') ) {
+    function karaoke_widgets() {
+        // Footer-1
+        register_sidebar( array(
+            'name'      =>      esc_html__('Footer-1', 'karaoke'),
+            'id'        =>      'footer-1',
+            'descripton'=>      esc_html__('Add widgets here', 'karaoke'),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</section>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        ) );
+
+        // Footer-2
+        register_sidebar( array(
+            'name'      =>      esc_html__('Footer-2', 'karaoke'),
+            'id'        =>      'footer-2',
+            'descripton'=>      esc_html__('Add widgets here', 'karaoke'),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</section>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        ) );
+
+        // Footer-3
+        register_sidebar( array(
+            'name'      =>      esc_html__('Footer-3', 'karaoke'),
+            'id'        =>      'footer-3',
+            'descripton'=>      esc_html__('Add widgets here', 'karaoke'),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</section>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        ) );
+
+        // Copyright
+        register_sidebar( array(
+            'name'      =>      esc_html__('Copyright', 'karaoke'),
+            'id'        =>      'copyright',
+            'descripton'=>      esc_html__('Add widgets here', 'karaoke'),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</section>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        ) );
+    }
+    add_action( 'widgets_init', 'karaoke_widgets' );
 }
